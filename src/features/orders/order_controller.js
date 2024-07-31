9217ffa82a1f3538d94b40e3690a10c4bf60d675
Amit kumar <codes.amitkumar@gmail.com>
2024-07-31 21:32:45 +0530

    payment gate way work-6 complete setup in controller is done

--- src/features/orders/order_controller.js
+++ src/features/orders/order_controller.js
@@ -4,7 +4,6 @@ import UserModel from "../users/user_model.js";
 export const paymentController = async (request, response) => {
   try {
     const { cartItems } = request.body;
-    console.log("cartItems data : ",cartItems);
     const user = await UserModel.findOne({ _id: request.userId });
     const params = {
       submit_type: "pay",
@@ -17,16 +16,29 @@ export const paymentController = async (request, response) => {
         },
       ],
       customer_email: user.email,
-      line_items:cartItems.map((items,index)=>{
-        return{
-            price_Data:{
-                currency:'inr',
-                product_data:{
-                    
-                }
-            }
+      line_items: cartItems.map((items, index) => {
+        return {
+          price_Data: {
+            currency: "inr",
+            product_data: {
+              name: items.productId.productName,
+              images: items.productId.productImage,
+              metadata: {
+                productId: items.productId._id,
+              },
+            },
+            unit_amount: items.productId.selling,
+          },
+          adjustable_quantity: {
+            enabled: true,
+            minimum:1
+          },
+          quantity:items.quantity
         }
-      })
+
+      }),
+      success_url:`${process.env.FRONTEND_DOMAIN}/success`,
+      cancel_url:`${process.env.FRONTEND_DOMAIN}/cancel`,
     };
 
     const session = await stripe.checkout.sessions.create(params);

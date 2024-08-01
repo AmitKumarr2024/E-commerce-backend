e8db29a87e4a36f758cc121469876dd531d4857f
Amit kumar <codes.amitkumar@gmail.com>
2024-08-01 21:39:15 +0530

    payment setup-3

--- src/features/orders/order_controller.js
+++ src/features/orders/order_controller.js
@@ -1,5 +1,6 @@
 import stripe from "../../config/strip.js";
 import UserModel from "../users/user_model.js";
+import order_module from "./order_module.js";
 
 // this is secret key
 const endpointSecret = process.env.STRIPE_END_POINT_SECRET_KEY;
@@ -58,6 +59,28 @@ export const paymentController = async (request, response) => {
   }
 };
 
+// function retrive the data as per input recieve
+const getLineItems = async (lineItems) => {
+  const ProductItems = [];
+
+  if (lineItems?.data?.length) {
+    for (const item of lineItems.data) {
+      const product = await stripe.products.retrieve(item.price.product);
+      const productId = product.metadata.productId;
+
+      const productData = {
+        productId: productId,
+        name: product.name,
+        price: item.price.unit_amount / 100,
+        quantity: item.quantity,
+        image: product.image,
+      };
+      ProductItems.push(productData);
+    }
+  }
+  return ProductItems;
+};
+
 export const webhooks = async (request, response) => {
   try {
     const sig = request.headers["stripe-signature"];
@@ -83,12 +106,30 @@ export const webhooks = async (request, response) => {
     }
 
     switch (event.type) {
-      case 'checkout.session.completed':
+      case "checkout.session.completed":
         const session = event.data.object;
-        const lineItems=await strip.checkout.sessions.listLineItems(session.id);
-        
+        const lineItems = await strip.checkout.sessions.listLineItems(
+          session.id
+        );
+        const productDetails = await getLineItems(lineItems);
 
-       
+        const OrderDetails = {
+          productDetails: productDetails,
+          email: session.customer_email,
+          userId: session.metadata.userId,
+          paymentDetails: {
+            paymentId: session.payment_intent,
+            payment_method_type: session.payment_method_types,
+            payment_status: session.payment_status,
+          },
+          shipping_options:session.shipping_options,
+          totalAmount:session.amount_total/100,
+        };
+
+        const order = await order_module(OrderDetails);
+
+        const saveOrder = await order.save();
+
         break;
       // ... handle other event types
       default:

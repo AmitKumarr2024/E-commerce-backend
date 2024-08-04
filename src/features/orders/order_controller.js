83eb6b7dc7335b175d7e4086c76a118daf25e7b6
Amit kumar <codes.amitkumar@gmail.com>
2024-08-04 15:53:11 +0530

    done-2

--- src/features/orders/order_controller.js
+++ src/features/orders/order_controller.js
@@ -5,7 +5,7 @@ import order_module from "./order_module.js";
 import CartModel from "../cart/cart_model.js";
 import Cancellation from "../orders/orderCancel_model.js";
 import user_model from "../users/user_model.js";
-
+import sendOrderConfirmationEmail from "../../helper/sendOrderConfirmationEmail.js";
 // this is secret key
 const endpointSecret = process.env.STRIPE_END_POINT_SECRET_KEY;
 
